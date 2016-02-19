e93e8fe471987e523bd9e23d9dd7f0ad69117459
Douglas Thompson <majesticromayne@gmail.com>
2016-02-18 17:28:21 -0700

    Pause Menu up!

---
+++ Through Her Eyes/Through Her Eyes/Assets/Scripts/UIManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public bool paused;
+	// Use this for initialization
+	void Start ()
+    {
+        paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(paused)
+        {
+            gamePaused(true);
+        }
+        else
+        {
+            gamePaused(false);
+        }
+        if(Input.GetButtonDown("Cancel"))
+        {
+            pauseSwitch();
+        }
+	}
+
+    private void gamePaused(bool state)
+    {
+        if(state)
+        {
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            Time.timeScale = 1.0f;
+        }
+        PausePanel.SetActive(state);
+    }
+
+    public void pauseSwitch()
+    {
+        if(paused)
+        {
+            paused = false;
+        }
+        else
+        {
+            paused = true;
+        }
+    }
+}
